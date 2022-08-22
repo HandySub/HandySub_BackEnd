1b2911a8618183f07e19e1ef890d524d63eeac80
lightorange0v0 <yeonjoo99@gmail.com>
2022-08-23 04:34:29 +0900

    Feat: 스프링 시큐리티 + JWT 진행중

--- src/main/java/com/example/handySub/domain/user/service/UserService.java
+++ src/main/java/com/example/handySub/domain/user/service/UserService.java
@@ -1,32 +1,10 @@
 package com.example.handySub.domain.user.service;
 
 import com.example.handySub.domain.user.dto.UserDto;
-import com.example.handySub.domain.user.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-@Service
-public class UserService implements UserServiceImpli {
-    @Autowired
-    UserRepository userRepository;
-
-    @Override
-    public void userInsert(UserDto userDto){
-        userRepository.userInsert(userDto);
-    }
-
-    @Override
-    public void userInfoUpdate(String email, String newinfo){
-        userRepository.userInfoUpdate(email, newinfo);
-    }
-
-    @Override
-    public void userDelete(String email){
-        userRepository.userDelete(email);
-    }
-
-    @Override
-    public UserDto loginCheck(String email, String pwd){
-        return userRepository.loginCheck(email,pwd);
-    }
+public interface UserService {
+    void userInsert(UserDto userDto); //유저 등록
+    void userInfoUpdate(String email, String newinfo); //유저 정보 수정
+    void userDelete(String email); //유저 삭제
+    UserDto loginCheck(String email, String pwd);
 }
